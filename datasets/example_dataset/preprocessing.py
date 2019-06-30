00e0a75b2c7695173256b6f310212924687e2cd9
TamarKl <32865687+TamarKl@users.noreply.github.com>
2019-06-30 21:50:04 +0300

    Update preprocessing.py
    adapt the pre_processing script to my own data

--- datasets/example_dataset/preprocessing.py
+++ datasets/example_dataset/preprocessing.py
@@ -25,11 +25,11 @@ from datasets.utils import reshape
 from utilities.file_and_folder_operations import subfiles
 
 
-def preprocess_data(root_dir, y_shape=64, z_shape=64):
+def preprocess_data(root_dir, y_shape=256, z_shape=64):
     image_dir = os.path.join(root_dir, 'imagesTr')
     label_dir = os.path.join(root_dir, 'labelsTr')
     output_dir = os.path.join(root_dir, 'preprocessed')
-    classes = 3
+    classes = 2
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -38,7 +38,7 @@ def preprocess_data(root_dir, y_shape=64, z_shape=64):
     class_stats = defaultdict(int)
     total = 0
 
-    nii_files = subfiles(image_dir, suffix=".nii.gz", join=False)
+    nii_files = subfiles(image_dir, suffix=".nii", join=False)
 
     for i in range(0, len(nii_files)):
         if nii_files[i].startswith("._"):
@@ -56,14 +56,26 @@ def preprocess_data(root_dir, y_shape=64, z_shape=64):
 
         # normalize images
         image = (image - image.min())/(image.max()-image.min())
+        # not original!!
+        print("********************\n")
+        print(image.shape)
+        print(label.shape)
+        image = reshape(image, append_value=0, new_shape=(256, 256, image.shape[2]))
+        label = reshape(label, append_value=0, new_shape=(256, 256, image.shape[2]))
+        # end of not original!!
+        #original!!!
+        #image = reshape(image, append_value=0, new_shape=(image.shape[0], y_shape, z_shape))
+        #label = reshape(label, append_value=0, new_shape=(label.shape[0], y_shape, z_shape))
 
-        image = reshape(image, append_value=0, new_shape=(image.shape[0], y_shape, z_shape))
-        label = reshape(label, append_value=0, new_shape=(label.shape[0], y_shape, z_shape))
-
+        #result = np.stack((image, label))
+        # not original!!
         result = np.stack((image, label))
-
+        result = np.swapaxes(result, 1, 3)
+        result = np.swapaxes(result, 2, 3)
+        # end of not original!!
         np.save(os.path.join(output_dir, f.split('.')[0]+'.npy'), result)
         print(f)
+
 
     print(total)
     for i in range(classes):
